7310622f14b4da0bcebe9844b8c9e9d12862a511
Paul <5540832@mail.ru>
2018-04-04 18:35:58 +0300

    HW1

---
+++ ProcessInformation/ProcessInformation/Programm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessInformation
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("http://localhost:4000/IContract");
+            server.OpenHost();
+
+            Console.WriteLine("Application ready for get message!!!");
+
+            //service.Say();
+
+            Console.ReadKey();
+            server.CloseHost();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//Написать консольное .NET приложение, позволяющее пользователю получить:
+//1. Список всех процессов
+//2. Выбрать процесс по PID
+//3. Запустить процесс
+//4. Оставить процесс
+//5. Показать информацию о потоках
+//6. Показать информацию о модулях
+//Предоставить пользователю меню выбора (по пунктам).
---
+++ ProcessInformation/ProcessInformation/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessInformation
+{
+    public class Server
+    {
+        private Uri _adress;
+        private BasicHttpBinding _binding;
+        private Type _contract;
+        private ServiceHost _host;
+
+        public Server(string localHost)
+        {
+            _adress = new Uri(localHost);
+            _binding = new BasicHttpBinding();
+            _contract = typeof(IContract);
+            _host = new ServiceHost(typeof(Service));
+            _host.AddServiceEndpoint(_contract, _binding, _adress);
+        }
+
+        public void OpenHost()
+        {
+            _host.Open();
+        }
+
+        public void CloseHost()
+        {
+            _host.Close();
+        }
+    }
+}
