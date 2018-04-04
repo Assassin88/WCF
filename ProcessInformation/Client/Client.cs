fb2b6f6eeab86911be7c08fc6a760619731da023
Paul <5540832@mail.ru>
2018-04-04 18:26:41 +0300

    WCF_HW_1

---
+++ ProcessInformation/Client/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        private Uri _adress;
+        private BasicHttpBinding _binding;
+        private EndpointAddress _endpointAdress;
+        private ChannelFactory<IContract> _factory;
+        private IContract _channel;
+
+        public Client(string adress)
+        {
+            _adress = new Uri(adress);
+            _binding = new BasicHttpBinding();
+            _endpointAdress = new EndpointAddress(_adress);
+            _factory = new ChannelFactory<IContract>(_binding, _endpointAdress);
+        }
+
+        public void GetInformation()
+        {
+            _channel = _factory.CreateChannel();
+            while (true)
+            {
+                var value = Menu.ShowMenu();
+                var responce = _channel.Say(value);
+                Console.WriteLine(responce);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ProcessInformation/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client Client = new Client("http://localhost:4000/IContract");
+            Client.GetInformation();
+        }
+    }
+}
